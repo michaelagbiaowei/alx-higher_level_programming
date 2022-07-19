2a39a1a792403d638eee949aac57ed132be459bd
bryanwalton <101793657+bryanwalton@users.noreply.github.com>
2022-07-19 21:58:39 +0100

    f

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -14,4 +14,5 @@ class Square:
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
-            raise 
+            raise ValueError("size must be >= 0")
+        self.__size = size
